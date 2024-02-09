ab812987eacf99c1c35dcc75885ac5c377ecee0e
wael salameh <90796176+superwae@users.noreply.github.com>
2024-02-09 14:37:51 +0200

    ViewModels

--- Backend/Lafatkotob.API/Lafatkotob/ViewModels/BadgeModel.cs
+++ Backend/Lafatkotob.API/Lafatkotob/ViewModels/BadgeModel.cs
@@ -1,6 +1,14 @@
-﻿namespace Lafatkotob.ViewModels
+﻿using Lafatkotob.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lafatkotob.ViewModels
 {
     public class BadgeModel
     {
+        public int Id { get; set; }
+        public string BadgeName { get; set; }
+        public string Description { get; set; }
+
+      
     }
 }
