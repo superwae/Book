009a82ecd6426d48c34f97e6ce57b338f8687537
wael salameh <90796176+superwae@users.noreply.github.com>
2024-03-05 13:32:33 +0200

    FIXS

--- Frontend/Lafatkotob-SPA/src/app/Home/components/home-page/home-page.component.ts
+++ Frontend/Lafatkotob-SPA/src/app/Home/components/home-page/home-page.component.ts
@@ -5,20 +5,27 @@ import { BooksComponent } from '../../../Book/components/books/books.component';
 import { BookService } from '../../../Book/Service/BookService';
 import { Book } from '../../../Book/Models/bookModel';
 import { MagicTextComponent } from '../magic-text/magic-text.component';
+import { AppUsereService } from '../../../Auth/services/app-user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RecommendationComponent,SearchBarComponent,BooksComponent,MagicTextComponent],
+  imports: [RecommendationComponent,SearchBarComponent,BooksComponent,MagicTextComponent,CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent implements OnInit {
   searchResults: Book[] = [];
+  isLoggedIn:boolean = false;
 
-  constructor(private bookService: BookService) {}
+  constructor(
+    private bookService: BookService,
+    private AppUsereService:AppUsereService)
+   {}
 
   ngOnInit(): void {
+    this.isLoggedIn=this.AppUsereService.isLoggedIn();
   }
 
   handleSearch(query: string): void {
