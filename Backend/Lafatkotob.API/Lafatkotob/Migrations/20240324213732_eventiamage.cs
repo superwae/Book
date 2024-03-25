1153c650177b5fb17439ef3c826f58d1de78cfe9
lana abdelrahman a bdalfatah <145432679+lanaabed@users.noreply.github.com>
2024-03-25 02:07:07 +0200

    edit event
    event and what .............

---
+++ Backend/Lafatkotob.API/Lafatkotob/Migrations/20240324213732_eventiamage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lafatkotob.Migrations
+{
+    /// <inheritdoc />
+    public partial class eventiamage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
